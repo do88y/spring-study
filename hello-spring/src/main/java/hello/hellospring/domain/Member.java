09261eb14a8f16dee515dbd0c5ad601f2da95ef8
do88y <appassionato.seo@gmail.com>
2022-09-29 23:58:34 +0900

    JPA 실습

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // IDENTITY-> DB가 알아서 생성해 주는 것
     private Long id;  // 고객이 만드는 id가 아니라 시스템이 정하는 임의의 값
     private String name;
 
