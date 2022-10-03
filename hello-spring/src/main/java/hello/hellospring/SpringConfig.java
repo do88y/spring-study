33a92c38ff456ab09cb5200d18734812dba4e4ad
do88y <appassionato.seo@gmail.com>
2022-10-03 23:48:43 +0900

    AOP 적용

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,37 +1,53 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+//import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//  JPA 관련 코드
+/*
+    private EntityManager em;
 
-    @Autowired  // 생성자 만들어서 오토와이어드 애너테이션
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+*/
+
+    private final MemberRepository memberRepository;
+
+    // 인젝션 받으면 스프링 데이터 JPA가 구현체를 만들어놓은게 등록됨
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+
+
+//     스프링 데이터 JPA 쓰면 스프링이 알아서 bean에 등록 해 줘서 필요 X
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();  // DB 연결 안 한 파일
 //        return new JdbcMemberRepository(dataSource);  // 순수 JDBC 실행
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+//        return new JdbcTemplateMemberRepository(dataSource);  // JDBC 템플릿
+//        return new JpaMemberRepository(em);
+//    }
+
 }
 
 
