cb09257bac97bd6e65c211c148ff9f98a739ab09
do88y <appassionato.seo@gmail.com>
2022-10-20 23:20:46 +0900

    주문 기능 테스트

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -104,7 +104,7 @@ public class Order {
     public int getTotalPrice() {
         int totalPrice = 0;
         for (OrderItem orderItem : orderItems) {
-            totalPrice += orderItem.getOrder().getTotalPrice();
+            totalPrice += orderItem.getTotalPrice();
         }
         return totalPrice;
     }
