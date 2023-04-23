e96364fba3533235c21b256c458f7b8840fb1ed1
do88y <appassionato.seo@gmail.com>
2023-04-23 19:08:39 +0900

    Querydsl - 스프링 데이터 JPA가 제공하는 Querydsl 기능

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,11 +2,13 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -20,7 +22,7 @@ import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
-public class MemberRepositoryImpl implements MemberRepositoryCustom{
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
@@ -74,6 +76,33 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
 
         return new PageImpl<>(content, pageable, total);
     }
+
+    /**
+     * QuerydslRepositorySupport 활용
+     */
+/*    public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
+
+        JPQLQuery<MemberTeamDto> jpaQuery = from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")));
+
+        //offset과 limit를 뺄 수 있지만 메서드 체인이 끊기고 따로 처리를 해줘야 하는 단점이 있음
+        List<MemberTeamDto> content = getQuerydsl().applyPagination(pageable, jpaQuery)
+                .fetch();
+
+        return PageableExecutionUtils.getPage(content, pageable, jpaQuery::fetchCount);
+    }*/
 
     /**
      * 복잡한 페이징
