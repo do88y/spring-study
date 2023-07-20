a7b4ad61749679325d266de33fec2f327481d433
DOBBY <102232306+do88y@users.noreply.github.com>
2023-07-20 22:40:01 +0900

    체크 예외 활용

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -0,0 +1,50 @@
+package hello.jdbc.exception.basic;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+public class CheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        //체크 예외를 처리하지 못해서 밖으로 던지기 위해 throws SQLException, ConnectException 선언 -> 예외를 의존해야 하는 문제
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        //체크 예외를 처리하지 못해서 밖으로 던지기 위해 throws SQLException, ConnectException 선언-> 예외를 의존해야 하는 문제
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+}
