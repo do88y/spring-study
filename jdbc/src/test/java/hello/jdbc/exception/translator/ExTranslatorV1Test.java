700881f78e135423b9e426f58d739b44bc373565
DOBBY <102232306+do88y@users.noreply.github.com>
2023-08-01 23:23:50 +0900

    데이터 접근 예외 직접 만들기 - test

--- jdbc/src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
+++ jdbc/src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -1,10 +1,14 @@
 package hello.jdbc.exception.translator;
 
+import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.ex.MyDBException;
 import hello.jdbc.repository.ex.MyDuplicateKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
@@ -13,8 +17,26 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Random;
 
+import static hello.jdbc.connection.ConnectionConst.*;
+
 @Slf4j
 public class ExTranslatorV1Test {
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("myId");
+        service.create("myId");//같은 ID 저장 시도
+    }
 
     @Slf4j
     @RequiredArgsConstructor
@@ -62,11 +84,11 @@ public class ExTranslatorV1Test {
                 if (e.getErrorCode() == 23505) {
                     throw new MyDuplicateKeyException(e);
                 }
+                throw new MyDBException(e);
             } finally {
                 JdbcUtils.closeStatement(pstmt);
                 JdbcUtils.closeConnection(con);
             }
         }
     }
-
 }
